d0bad420168e111e3cdc110f7b17ebe2a60934f9
unknown <JNKarif>
2024-08-31 15:31:45 +0600

    animation added

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ let qrImage = document.getElementById("qr-image");
 let qrText = document.getElementById("qr-text");
 
 function generateQR() {
-  qrImage.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrText.value;
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+
+    imgBox.classList.add("show-img");
+  } else {
+    alert("Please, type your text or URL!");
+  }
 }
